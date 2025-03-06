722e23aac1e8a63419f0f48f7435313676266c66
Akash Pandey <akashpande027@gmail.com>
2025-03-06 20:35:30 +0530

    Akash Pandey UC3: implemented DTO

---
+++ src/main/java/com/project/AddressBookAppDevelopment/dto/AddressDTO.java
@@ -0,0 +1,21 @@
+package com.project.AddressBookAppDevelopment.dto;
+
+public class AddressDTO {
+    private String name;
+    private String email;
+    private String phone;
+    private String city;
+
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+}
