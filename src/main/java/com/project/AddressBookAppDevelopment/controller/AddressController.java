f00607432c8b2fca7e84ddb6251382d48aab850e
Akash Pandey <akashpande027@gmail.com>
2025-03-06 20:23:09 +0530

    Akash Pandey UC1: request API

---
+++ src/main/java/com/project/AddressBookAppDevelopment/controller/AddressController.java
@@ -0,0 +1,56 @@
+package com.project.AddressBookAppDevelopment.controller;
+
+import com.project.AddressBookAppDevelopment.dto.AddressDTO;
+import com.project.AddressBookAppDevelopment.model.Address;
+import com.project.AddressBookAppDevelopment.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/addresses")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    // Create Address
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody AddressDTO addressDTO) {
+        Address savedAddress = addressService.addAddress(addressDTO);
+        return ResponseEntity.ok(savedAddress);
+    }
+
+    // Get All Addresses
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        return ResponseEntity.ok(addressService.getAllAddresses());
+    }
+
+    // Get Address by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Address>> getAddressById(@PathVariable Long id) {
+        return ResponseEntity.ok(addressService.getAddressById(id));
+    }
+
+    // Update Address
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody AddressDTO addressDTO) {
+        Address updatedAddress = addressService.updateAddress(id, addressDTO);
+        if (updatedAddress != null) {
+            return ResponseEntity.ok(updatedAddress);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete Address
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAddress(@PathVariable Long id) {
+        addressService.deleteAddress(id);
+        return ResponseEntity.ok("Address deleted successfully");
+    }
+}
---
+++ src/main/java/com/project/AddressBookAppDevelopment/service/AddressService.java
@@ -0,0 +1,57 @@
+package com.project.AddressBookAppDevelopment.service;
+
+import com.project.AddressBookAppDevelopment.dto.AddressDTO;
+import com.project.AddressBookAppDevelopment.model.Address;
+import com.project.AddressBookAppDevelopment.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    // Create a new address
+    public Address addAddress(AddressDTO addressDTO) {
+        Address address = new Address(
+                addressDTO.getName(),
+                addressDTO.getEmail(),
+                addressDTO.getPhone(),
+                addressDTO.getCity()
+        );
+        return addressRepository.save(address);
+    }
+
+    // Retrieve all addresses
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    // Get Address by ID
+    public Optional<Address> getAddressById(Long id) {
+        return addressRepository.findById(id);
+    }
+
+    // Update Address
+    public Address updateAddress(Long id, AddressDTO addressDTO) {
+        Optional<Address> existingAddress = addressRepository.findById(id);
+        if (existingAddress.isPresent()) {
+            Address address = existingAddress.get();
+            address.setName(addressDTO.getName());
+            address.setEmail(addressDTO.getEmail());
+            address.setPhone(addressDTO.getPhone());
+            address.setCity(addressDTO.getCity());
+            return addressRepository.save(address);
+        }
+        return null;
+    }
+
+    // Delete Address
+    public void deleteAddress(Long id) {
+        addressRepository.deleteById(id);
+    }
+}
